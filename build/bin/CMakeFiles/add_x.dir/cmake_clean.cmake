c96a14e7befe0faac8f2275ce4660c41a5ba6919
wuli133144 <1683358846@qq.com>
2017-11-14 11:53:31 +0800

    new files

---
+++ build/bin/CMakeFiles/add_x.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/add_x.dir/main.o"
+  "CMakeFiles/add_x.dir/utils.o"
+  "add_x.pdb"
+  "add_x"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang C)
+  INCLUDE(CMakeFiles/add_x.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
